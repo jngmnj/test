8ce0ab6f94390a489b2c1ac0dcf5e17ed1177936
jngmnj <58453631+jngmnj@users.noreply.github.com>
2019-12-03 13:25:06 +0900

    Update welcome.js
    test

--- welcome.js
+++ welcome.js
@@ -1,5 +1,5 @@
 var subject1 = "Welcom to Javascript!<br>";
-var subject2 = "Enjoy Javascript programming!";
+var subject2 = "Enjoy Javascript programming! test-edit";
 function welcome(target){
     document.write(target);
 }
