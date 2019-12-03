40b3dd3cc7c26f35849f4accbd44b494cbfd15bd
jngmnj <exorbitance@naver.com>
2019-12-03 13:04:56 +0900

    파일업로드 1
    파일업로드 1

---
+++ welcome.js
@@ -0,0 +1,8 @@
+var subject1 = "Welcom to Javascript!<br>";
+var subject2 = "Enjoy Javascript programming!";
+function welcome(target){
+    document.write(target);
+}
+
+welcome(subject1);
+welcome(subject2);
